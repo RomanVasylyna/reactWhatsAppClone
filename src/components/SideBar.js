822d4d749eeee5812778f1f850c4a47b4e3cb368
Roman <romanvasylyna888@gmail.com>
2021-07-29 17:40:34 +0300

    Sidebar Tabs

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,26 +1,39 @@
-import React, { useRef } from 'react';
-// import {  } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Nav, Tab } from 'react-bootstrap';
 
 const SideBar = ({ userID }) => {
 
-    const idRef = useRef('');
+    const CONVERSATIONS_KEY = 'conversations';
+    const CONTACTS_KEY = 'contacts';
+
+    const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY);
     
     return (
-        <div>
-            <div className="d-flex align-items-start">
-                <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <button className="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
-                    <button className="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
-                    <button className="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
-                    <button className="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
-                </div>
-                <div className="tab-content" id="v-pills-tabContent">
-                    <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
-                    <div className="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-                    <div className="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-                    <div className="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
-                </div>
-            </div>
+        <div style={{ width: '250px'}} className="d-flex flex-column">
+         <Tab.Container activeKey={activeKey}>
+         <Nav variant="tabs" className="justify-content-center">
+             <Nav.Item>
+                 <Nav.Link eventKey={CONVERSATIONS_KEY} onClick={() => setActiveKey(CONVERSATIONS_KEY)}>
+                     Conversations
+                 </Nav.Link>
+             </Nav.Item>
+
+             <Nav.Item>
+                 <Nav.Link eventKey={CONTACTS_KEY} onClick={() => setActiveKey(CONTACTS_KEY)}>
+                     Contacts
+                 </Nav.Link>
+             </Nav.Item>
+
+         </Nav>
+         
+         {/* Content Inside Tabs*/}
+         <Tab.Content>
+          <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+
+          </Tab.Pane>
+         </Tab.Content>
+
+         </Tab.Container>
         </div>
     )
 }
