1d6005cff7d6aa4b5e83448e66d79972a52b6395
Roman <romanvasylyna888@gmail.com>
2021-08-13 18:04:25 +0300

    Conversation Output

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -6,6 +6,8 @@ const Conversations = () => {
 
     const { conversations } = useConversation();
 
+    console.log(conversations.map(conversation => conversation.recipients.map(recipient => recipient.recipientID)));
+
     return (
         <ListGroup variant="flush">
             {conversations.length ? conversations.map(recipient =>
@@ -13,6 +15,7 @@ const Conversations = () => {
                     <span>{recipient.contactName}</span>
                 </ListGroup.Item>) :<p>You have no conversations yet...</p> }
         </ListGroup>
+
         // <ListGroup variant="flush">
         // {conversations.length ? conversations.recipients.map(recipient =>
         //     <ListGroup.Item key={recipient.recipientID}>
