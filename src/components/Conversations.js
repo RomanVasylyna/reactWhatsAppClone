8d2b83b9c3fdf99590057ebdb1091ec2ff5f2781
Roman <romanvasylyna888@gmail.com>
2021-08-17 17:46:20 +0300

    conversations layout

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, ListGroup } from 'react-bootstrap';
 import { useConversation } from '../contexts/ConversationProvider';
 
@@ -6,37 +6,17 @@ const Conversations = () => {
 
     const { conversations } = useConversation();
 
-    console.log(conversations.map(conversation => conversation.newConversation.map(conv => conv.contactName).join(',')));
-
-    // console.log(conversations.map(conversation => conversation.recipients.map(recipient => recipient.contactName)));
-
     return (
 
-        <p>You have no conversations yet...</p>
-        // <ListGroup variant="flush">
-        //     {conversations.length ?
-        //         conversations.map(conversation =>
-        //             conversation.newConversation.map(conv =>
-        //                 conv.contactName).join(',').map((el, index) =>
-        //                     <ListGroup.Item key={index}>
-        //                         {el}
-        //                     </ListGroup.Item>
-        //                 ))
-        //         : <p>You have no conversations yet...</p>}
-        // </ListGroup>
+        <ListGroup variant="flush">
+            {conversations.length ?
+                conversations.map(conversation => conversation.newConversation.map(conversation => conversation.contactName).join(',')).map((el, index) =>
+                    <ListGroup.Item key={index}>
+                        {el}
+                    </ListGroup.Item>
+                ) : <p>You have no conversations yet...</p>}
+        </ListGroup>
 
-        // <ListGroup.Item key={conversation.recipients.recipientID}>
-        //     <span>{conversation.recipients.contactName}</span>
-        // </ListGroup.Item>) :<p>You have no conversations yet...</p> }
-
-
-        // <ListGroup variant="flush">
-        // {conversations.length ? conversations.recipients.map(recipient =>
-        //     <ListGroup.Item key={recipient.recipientID}>
-        //         <span>{recipient.contactName}</span>
-        //     </ListGroup.Item>
-        // ) : <p>You have no conversations yet...</p>}
-        // </ListGroup>
     )
 }
 
