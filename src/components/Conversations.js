93947ff7c7252064d337b23e3a379bfc593e545c
Roman <romanvasylyna888@gmail.com>
2021-08-18 17:49:17 +0300

    selected index

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -4,15 +4,25 @@ import { useConversation } from '../contexts/ConversationProvider';
 
 const Conversations = () => {
 
-    const { conversations } = useConversation();
+    const { conversations, selectConversationIndex } = useConversation();
+    // 1. передаем индекс текущего разговора в функцию по клику
+    // 2. Функция сопоставляет индексы (находит нужный разговор)
+    // 3. добавляем свойство selected в объект с разговором совпадающим с индексом
+    // 4. Если conversation.selected = true то элемент active (синий)
+
+    // active={conversation.selected}
+    // onClick={() => selectConversationIndex(index)
 
     return (
 
         <ListGroup variant="flush">
             {conversations.length ?
-                conversations.map(conversation => conversation.newConversation.map(conversation => conversation.contactName).join(',')).map((el, index) =>
-                    <ListGroup.Item key={index}>
-                        {el}
+                conversations.map(conversation => conversation.newConversation.map(conversation => conversation.contactName).join(',')).map((name, index) =>
+                    <ListGroup.Item 
+                    key={index} 
+                    action
+                    onClick={() => selectConversationIndex(index)}>
+                        {name}
                     </ListGroup.Item>
                 ) : <p>You have no conversations yet...</p>}
         </ListGroup>
--- src/contexts/ConversationProvider.js
+++ src/contexts/ConversationProvider.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 import { useContacts } from '../contexts/ContactProvider';
 
@@ -15,12 +15,18 @@ export const ConversationProvider = ({ children }) => {
     const { contacts } = useContacts();
     const [conversations, setConversations] = useLocalStorage('conversations', []);
 
+    const [selectedConversationIndex, setSelectedConversationIndex] = useLocalStorage('activeConversation', []);
+
+    const selectConversationIndex = (index) => {
+       return setSelectedConversationIndex(conversations.filter((el, ind) => ind === index));
+    }
+
     const createConversation = (ids) => {
-        return setConversations([...conversations, { newConversation: ids.map(id => { return { recipientID: id, contactName: contacts.filter(contact => contact.id === id)[0].name, messages: [] } }) }]);
+        return setConversations([...conversations, { newConversation: ids.map(id => { return { recipientID: id, contactName: contacts.filter(contact => contact.id === id)[0].name, messages: [] } }), selected: false}]);
     }
 
     return (
-        <ConversationContext.Provider value={{ conversations, createConversation }}>
+        <ConversationContext.Provider value={{ conversations, createConversation, selectConversationIndex }}>
             {children}
         </ConversationContext.Provider>
     )
