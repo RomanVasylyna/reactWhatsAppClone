276c7ae6dd31e258edd2070bf72c4bbf933a00d0
Roman <romanvasylyna888@gmail.com>
2021-10-05 23:21:16 +0300

    if all bool

--- src/contexts/ConversationProvider.js
+++ src/contexts/ConversationProvider.js
@@ -19,8 +19,10 @@ export const ConversationProvider = ({ children, userID }) => {
     const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
 
     const [conversationIdsIndex, setConversationIdsIndex] = useState(0);
-    
+
+    // Set Test State
     const[test, setTest] = useState(false);
+    const[testTwo, setTestTwo] = useState(false);
 
     const sendMessage = text => {
         const currentConversation = conversations.filter(conversation => conversation.selected)[0];
@@ -60,19 +62,40 @@ export const ConversationProvider = ({ children, userID }) => {
 
         // const allValuesMatch = ids.every((id, index) => id === conversationIds[index]);
         // при первом true новая конфа не должна создаться
-        
+
         // true/false
+        // setTestTwo(conversationIds.map(conversation => conversation.every(conv => ids.includes(conv))));// [true, true, true, true]
+        // const two = one.every(Boolean); //true
+
+        // one = array with booleans [true, true, true, true]
+        // two = if one (array with booleans) true return single true
+
+        // conversationIds.map(conversation => conversation.every(conv => ids.includes(conv))[0])[0]
+
+        // test = lastAdded conversation matched true/false
+        // testTwo = all conversations [true, true, false]
         setTest(conversationIds.map(conversation => conversation.every(conv => ids.includes(conv))[0])[0]);
-      
+        setTestTwo(conversationIds.map(conversation => conversation.every(conv => ids.includes(conv))));
+
         if (ids.length) {
-            if (!test) { //false
-                console.log(test);
+            if (!testTwo.every(Boolean)) { //false
                 setConversations([...conversations, { newConversation: ids.map(id => { return { recipientID: id, contactName: contacts.filter(contact => contact.id === id)[0].name } }), selected: false, messages: [], sender: userID }]);
                 setTest(true);
+                console.log(testTwo);
+                // console.log(one);
+                // console.log(two);
+                // console.log(dodik);
+                // console.log(ifBool);
             } else {
                 alert('You already have this conversation');
-                console.log(test);
-                console.log(conversationIds);
+                setTest(false);
+                setTestTwo([]);
+                // console.log(dodik);
+                // console.log(ifBool);
+                // console.log(test);
+                // console.log(one);
+                // console.log(two);
+                // console.log(conversationIds);
             }
         } else {
             alert('Please choose at least one recipient');
