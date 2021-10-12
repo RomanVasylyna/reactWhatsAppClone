e20056c252a3e201171f141ba5fd2cbec43de150
Roman <romanvasylyna888@gmail.com>
2021-10-12 23:43:17 +0300

    solution

--- src/contexts/ConversationProvider.js
+++ src/contexts/ConversationProvider.js
@@ -28,70 +28,77 @@ export const ConversationProvider = ({ children, userID }) => {
         return setConversations(conversations.map((el, ind) => ind === index ? { ...el, selected: true } : { ...el, selected: false }));
     }
 
+    const checkEquality = (arr1, arr2) => {
+        //If elements' lengths differ we immediately return false
+        // if (arr1.length !== arr2.length) return false;
+
+        // If first condition failed we sort both arrays
+        // arr1.sort();
+        // arr2.sort();
+
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i].length !== arr2.length) return false;
+            arr1[i].sort();
+            arr2.sort();
+            return arr1[i].every((el, index) => el === arr2[index]);
+        }
+
+
+        // And make sure that each element of one array equals the element of the second
+        // return arr1.filter(elem => elem.every((el, index) => el === arr2[index]));
+    }
+
     const removeConvDuplicates = ids => {
         //const newConv = { newConversation: ids.map(id => { return { recipientID: id, contactName: contacts.filter(contact => contact.id === id)[0].name } }), selected: false, messages: [], sender: userID };
         //let madeChange = false;
 
-    //     if(arraysAreEqual(conversationRecipients, recipients)) {
-    //     madeChange = true;
-    //     return {
-    //     [...conversation, ]
-    //     }
-    //     })
+        // if(a.length !== b.length) - сравниваем массивы по длине
 
-    //     if (madeChange) {
-    //         console.log('fdsfds');
-    //     } else {
-    //         setConversations([...conversations, { newConversation: ids.map(id => { return { recipientID: id, contactName: contacts.filter(contact => contact.id === id)[0].name } }), selected: false, messages: [], sender: userID }])
-    //     }
+        //     if(arraysAreEqual(conversationRecipients, recipients)) {
+        //     madeChange = true;
+        //     return {
+        //     [...conversation, ]
+        //     }
+        //     })
+
+        //     if (madeChange) {
+        //         console.log('fdsfds');
+        //     } else {
+        //         setConversations([...conversations, { newConversation: ids.map(id => { return { recipientID: id, contactName: contacts.filter(contact => contact.id === id)[0].name } }), selected: false, messages: [], sender: userID }])
+        //     }
     }
 
 
-
     const createConversation = (ids) => {
+        // Getting all recipients' ids from each individual conversation
         const conversationIds = conversations.map(conversation => conversation.recipients.map(conversation => conversation.id));
-        //const conversationIds = conversations.map(conversation => conversation.newConversation.map(conversation => conversation.recipientID));
 
-        const newConv = { recipients:ids.map(id => { return { id, name: contacts.filter(contact => contact.id === id)[0].name } }), messages:[], selected: false, sender: userID };
+        // Newly created conversation object
+        const newConv = { recipients: ids.map(id => { return { id, name: contacts.filter(contact => contact.id === id)[0].name } }), messages: [], selected: false, sender: userID };
 
-        //const newConvIds = newConv.newConversation.map(conv => conv.recipientID);
-
-        //setTest(conversationIds.map(conversation => conversation.every(conv => ids.includes(conv))[0])[0]);
-        // setTestTwo(conversationIds.map(conversation => conversation.every(conv => ids.includes(conv))));
-
-        let check = conversations.map(conversation => JSON.stringify(conversation) === JSON.stringify(newConv));
-        // let check2 = conversationIds.map(conversation => conversation.every(conv => ids.includes(conv)));
-
-
-        // let check3 = conversationIds.map(conversationId => conversationId.every(id => newConvIds.includes(id)));
+        // Making sure conversations objects are unique
+        let unique = conversations.map(conversation => JSON.stringify(conversation) === JSON.stringify(newConv));
 
         // сравнить newConvIds и каждую из conversationIds при помощи every
+        // !unique.some(Boolean)
+        // !checkEquality(ids, conversationIds);
 
-
-        if(ids.length) {
-            if (!check.some(Boolean)) { //false
+        if (ids.length) {
+            if (!checkEquality(conversationIds, ids)) { //false
                 setConversations([...conversations, newConv]);
-                // console.log(check);
-                // console.log(check2);
-                // console.log(newConvIds);
-                // console.log(conversationIds);
+                console.log(ids);
+                console.log(conversationIds);
+                // console.log(checkEquality(ids, conversationIds));
+                // console.log(conversationIds[1]);
                 //console.log(check3);
-                check=[];
+                unique = [];
             } else {
                 alert('You already have this conversation');
-                // console.log(testConv);
-                // console.log(newConv);
-                // console.log(newConvIds);
-                // console.log(conversationIds);
             }
         } else {
             alert('Please choose at least one recipient');
         }
 
-        // return ids.length && !singleMatch ?
-        //     setConversations([...conversations, { newConversation: ids.map(id => { return { recipientID: id, contactName: contacts.filter(contact => contact.id === id)[0].name } }), selected: false, messages:[], sender: userID }])
-        //     :
-        //     alert('Please choose at least one recipient');
     }
 
     return (
